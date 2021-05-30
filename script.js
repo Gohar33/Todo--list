6025cc059782a30fed526860118dd804d7dc7c38
Gohar33 <vangogik@gmail.com>
2021-05-31 00:15:51 +0200

    few changes

--- script.js
+++ script.js
@@ -1,62 +1,28 @@
 
 // first step to take ids of input and button of the Add section
 
-const label = document.getElementById("labelElement");
 const addInput = document.getElementById("inputElement")
 const addbutton = document.getElementById("buttonElement")
+const result = document.querySelector(".result")
 
-// second step is to take ids of The List section
-
-const listUl = document.getElementById("unorderedList");
-const completed = document.querySelector("ul")[1];
-
-// try to add new items by creating function
-
-
-function addItem() {
-  const li = document.createElement("li");
-  li.appendChild(document.createTextNode(addInput.value));
-  const checkBox = document.createElement("input");
-  checkBox.type = "checkbox";
-  checkBox.class = "checkbox";
-
-  const label = document.createElement("label");
-
-  const deleteButton = document.createElement("button");
-  deleteButton.class = "delete-2";
-  deleteButton.innerHTML = "Delete";
-  label.innerHTML = addInput.value;
-  li.appendChild(checkBox);
-  li.appendChild(label);
-  li.appendChild(deleteButton);
-  listUl.appendChild(li);
-}
-
-addbutton.addEventListener("click", addItem)
+addbutton.addEventListener("click", function buttonFunction() {
+  result.innerHTML = addInput.value;
+  result.innerHTML = `${value}`;
+  result.appendChild(addInput);
 
 
+});
+
+//buttonFunction()
 
 
-const list
-function createItems() {
-  const addValue = addInput.value
-  listUL.push
-  addInput.innerHTML = listUL
+const deleteButton = document.querySelector(".delete-2")
+const deletedInput = document.querySelector(".scratch")
+const listItem = document.getElementById("listItem")
 
-}
+deleteButton.addEventListener("click", function deletedItem() {
+  deletedInput.appendChild(listItem);
+});
 
-addbutton.addEventListener("click", createItems)
-
-function myFunction() {
-  const node = document.createElement("li");
-  const textnode = document.createTextNode("Water");
-  node.appendChild(textnode);
-  document.getElementById("myList").appendChild(node);
-}
-
-
-
-
-
-
+deletedItem()
 
